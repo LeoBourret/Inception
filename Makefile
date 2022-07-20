b432fc0c3cc9854149f2fbeb24d3a9b0f05d9015
Leo Bourret <bourretleo@hotmail.fr>
2022-07-20 10:38:19 -0400

    i believe it is done

--- Makefile
+++ Makefile
@@ -1,18 +1,18 @@
 all:
-	sudo mkdir -p /Users/lebourre/data
-	sudo mkdir -p /Users/lebourre/data/mysql
-	sudo mkdir -p /Users/lebourre/data/wp
-	sudo chmod -R 777 /Users/lebourre/data
+	sudo mkdir -p /home/lebourre/data
+	sudo mkdir -p /home/lebourre/data/mysql
+	sudo mkdir -p /home/lebourre/data/wp
+	sudo chmod -R 777 /home/lebourre/data
 	docker compose -f srcs/docker-compose.yaml up --build
 
 clean:
 	docker compose -f srcs/docker-compose.yaml down
 	docker container prune
 	docker image prune
-	docker volume prune
+	docker volume rm -f `docker volume ls`
 	docker network prune
-	sudo rm -rf /Users/lebourre/data/mysql
-	sudo rm -rf /Users/lebourre/data/wp
+	sudo rm -rf /home/lebourre/data/mysql
+	sudo rm -rf /home/lebourre/data/wp
 
 
 re: clean all
--- srcs/requirements/mariadb/set_db.sh
+++ srcs/requirements/mariadb/set_db.sh
@@ -9,20 +9,31 @@ then
 	echo "Database already exists"
 else
 
-mysql --user=root -p <<_EOF_
+echo "before heredoc"
 
-UPDATE mysql.user SET Password=('$DB_ROOT_PASSWORD') WHERE User='root';
+mysql --user=root --skip-password <<_EOF_
+
+ALTER USER root@localhost IDENTIFIED WITH mysql_native_password;
+ALTER USER root@localhost IDENTIFIED BY '$DB_ROOT_PASSWORD';
 DELETE FROM mysql.user WHERE User='';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DROP DATABASE test;
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';
 FLUSH PRIVILEGES;
 _EOF_
 
-echo "CREATE DATABASE IF NOT EXISTS $DATABASE; GRANT ALL ON $DATABASE.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
+
+echo "before pipe"
+
+echo "CREATE DATABASE IF NOT EXISTS $DATABASE; GRANT ALL ON $DATABASE.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot -p$DB_ROOT_PASSWORD
+
+sed -i "s/password = /password = $DB_ROOT_PASSWORD/g" /etc/mysql/debian.cnf
 
 fi
 
+echo "before stop"
+
 /etc/init.d/mysql stop
+
+echo "after stop"
 
 exec "$@"
